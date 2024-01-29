3c196d1e3f65a47f8f37ff2a5fcdf4b3919a1490
Nguyen Van Khanh An <khanhan7511@gmail.com>
2024-01-29 10:46:19 +0700

    Security

--- lib/config/api_keys.dart
+++ lib/config/api_keys.dart
@@ -1,2 +1,2 @@
-const String apiKey = "AIzaSyABdaSobxT_5InHf9tT28ETmXfki_mKAzM";
-const String contextKey = "a6408d9dd5e9e4184";
+const String apiKey = "Your API Key";
+const String contextKey = "Your Context Key";
